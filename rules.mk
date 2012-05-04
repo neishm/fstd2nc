349223cb9943169d676260143d9d89eca9ba73ca
Mike Neish <Michael.Neish@ec.gc.ca>
2012-05-04 13:36:47 -0400

    Copy (and prune) rules.mk from the main PyGeode distribution.
    The old symbolic link only worked if you checked out the repository directly
    into pygeode/plugins/.

--- rules.mk
+++ rules.mk
@@ -1,1 +1,43 @@
-../../rules.mk
+
+# Platform-specific settings
+ifeq ($(MAKE),mingw32-make)
+  # assume Windows
+  PLATFORM = WINDOWS
+  RM = del
+  LIBEXT = dll
+else
+  # assume Linux
+  PLATFORM = LINUX
+  RM = rm -f
+  LIBEXT = so
+endif
+
+all: $(PROGS) $(addsuffix .dir, $(SUBDIRS)) $(addprefix lib, $(addsuffix .$(LIBEXT), $(LIBS)))
+
+# GCC
+CC = gcc
+FC = gfortran
+CFLAGS += -std=c99 -fPIC -g -fbounds-check -Wall -D$(PLATFORM)
+FFLAGS += -fPIC -g -fbounds-check -Wall
+LDLIBS += -lm
+SHARED = -shared -Wl,-soname,$(basename $<)
+
+%: %.c
+	$(CC) $(CFLAGS) $(LDLIBS) -o $@ $^
+
+lib%.$(LIBEXT): %.c
+	$(CC) $(CFLAGS) $(LDLIBS) $(SHARED) -o $@ $^
+
+lib%.$(LIBEXT): %.f
+	$(FC) $(FFLAGS) $(LDLIBS) $(SHARED) -o $@ $^
+
+
+%.dir:
+	@(cd $(basename $@) && $(MAKE))
+
+%.clean:
+	@(cd $(basename $@) && $(MAKE) clean)
+
+clean: $(addsuffix .clean, $(SUBDIRS))
+	@( $(RM) $(PROGS) *.o *.$(LIBEXT) *.pyc && exit 0 )
+
