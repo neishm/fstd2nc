fc8d1a70c1659a3dc824cb39555166468924cfad
Mike Neish <Michael.Neish@ec.gc.ca>
2012-05-09 15:21:42 -0400

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 # PyGeode installation script
 
 setup (	name="python-pygeode-rpn",
-	version="0.0",
+	version="0.1.0",
         author="Mike Neish",
 	packages=["pygeode.plugins.rpn"]
 )
