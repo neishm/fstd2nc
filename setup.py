b24d776666a519fad391712fdb16290e90504a03
Mike Neish <Michael.Neish@ec.gc.ca>
2014-02-24 17:58:08 -0500

    Add numpy include directories to setup.py, so they can be found by the extension modules at compile time.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
+import numpy as np
 
 # Extension modules
 fstd_core = Extension ('pygeode.formats.fstd_core', sources=['fstd_core.c'], libraries=['rmn'])
@@ -12,6 +13,7 @@ setup (	name="python-pygeode-rpn",
 	version="0.8.3",
         author="Mike Neish",
 	ext_modules=[fstd_core, fstd_extern],
+        include_dirs = [np.get_include()],
 	packages=["pygeode.plugins.rpn","pygeode.formats"]
 )
 
