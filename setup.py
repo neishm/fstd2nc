06789c8f3577d0dcd866ab7d903e6a17a7d7f115
Mike Neish <neishm@atmosp.physics.utoronto.ca>
2016-07-11 10:39:52 -0400

    Release 1.1.6.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ fstd_extern = Extension ('pygeode.formats.fstd_extern', sources=['fstd_externmod
 # PyGeode installation script
 
 setup (	name="python-pygeode-rpn",
-	version="1.1.5",
+	version="1.1.6",
         author="Mike Neish",
 	ext_modules=[fstd_core, fstd_extern],
         include_dirs = [np.get_include()],
