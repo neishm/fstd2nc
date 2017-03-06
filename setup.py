71976cdc4589817495f4700cfd402fca8284b76a
Mike Neish <Michael.Neish@canada.ca>
2017-03-06 22:29:13 +0000

    Remove the old fstd_core extension module and the old fstd2nc script.
    The extension module is no longer needed (we're calling rpnpy.librmn directly),
    and the old script is superceded by the new fstd2nc module.
    
    Remove the old Changelog and update the README, since this now a standalone
    module that doesn't use PyGeode anymore.
    
    Note: still retaining some old PyGeode code in the directory for now, until all
    the old funtionality is ported over.

--- setup.py
+++ setup.py
@@ -1,20 +1,10 @@
 #!/usr/bin/env python
 
-from distutils.core import setup, Extension
-import numpy as np
+from distutils.core import setup
 
-# Extension modules
-fstd_core = Extension ('pygeode.formats.fstd_core', sources=['fstd_core.c'], libraries=['rmn'])
-
-# PyGeode installation script
-
-setup (	name="python-pygeode-rpn",
-	version="2.x-UNRELEASED",
+setup (	name="fstd2nc",
+	version="UNRELEASED",
         author="Mike Neish",
-	ext_modules=[fstd_core],
-        include_dirs = [np.get_include()],
-	packages=["pygeode.formats"],
-        package_dir = {"pygeode.formats":""},
-	scripts=["fstd2nc"]
+	py_modules=["fstd2nc"],
 )
 
