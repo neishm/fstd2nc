be05094fe65d07d5491f2b5d5e5e22189e71a503
Mike Neish <Michael.Neish@canada.ca>
2017-03-29 13:27:29 -0400

    Add license blurb.

--- fstd2nc.py
+++ fstd2nc.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python
 
+###############################################################################
+# Copyright 2017 - Climate Research Division
+#                  Environment and Climate Change Canada
+#
+# This file is part of the "PyGeode-RPN" package.
+#
+# "PyGeode-RPN" is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# "PyGeode-RPN" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with "PyGeode-RPN".  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+
+
 """
 Functionality for converting between FSTD and netCDF files.
 """
