682be54201e387f274dc162cf84d3de3bfc317c0
Mike Neish <Michael.Neish@canada.ca>
2017-07-05 16:16:14 -0400

    Switch to setuptools for installation.
    Also install command-line script 'fstd2nc'.
    
    Most of this was copied from a version of setup.py used to create the Python
    wheel.

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-from distutils.core import setup
+setup (
+  name="fstd2nc",
+  version="0.20170705",
+  description = 'Converts FSTD files (from Environment Canada) to netCDF files.',
+  url = 'https://github.com/neishm/fstd2nc',
+  author="Mike Neish",
+  license = 'LGPL-3',
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Scientific/Engineering :: Atmospheric Science',
+  ],
+  py_modules = ['fstd2nc'],
+  install_requires = ['numpy','pytz','netcdf4'],
+  package_data = {}, #TODO: language files
+  entry_points={
+    'console_scripts': [
+      'fstd2nc = fstd2nc:_fstd2nc_cmdline_trapped',
+    ],
+  },
 
-setup (	name="fstd2nc",
-	version="UNRELEASED",
-        author="Mike Neish",
-	py_modules=["fstd2nc"],
 )
 
