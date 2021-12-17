bb6b9e0aed6a28324be28a5d26b1bca8c73dc468
Mike Neish <Michael.Neish@ec.gc.ca>
2021-12-17 22:35:54 +0000

    Move fstdump code into fstd2nc/__main__.py.

--- fstd2nc/__main__.py
+++ fstd2nc/__main__.py
@@ -24,6 +24,53 @@ from fstd2nc.stdout import _, info, warn, error
 from fstd2nc import Buffer
 
 
+#################################################
+# Dump the metadata for an RPN standard file to stdout (without conversion
+# step).  Displays in format similar to "ncdump" for netCDF files.
+def _fstdump (buffer_type=Buffer):
+  import fstd2nc
+  from argparse import ArgumentParser
+  from sys import stdout, argv
+  from os.path import exists
+  from rpnpy.librmn.fstd98 import FSTDError, fstopt
+  parser = ArgumentParser(description=_("Display RPN standard file (FSTD) metadata in a structured format."))
+  parser.add_argument('infile', metavar='<fstd_file>', help=_('The RPN standard file to query.'))
+  parser.add_argument('-v', nargs='*', metavar='NOMVAR,...', help=_('Display the values for the specified variable.'))
+  buffer_type._cmdline_args(parser)
+  args = parser.parse_args()
+  buffer_type._check_args(parser, args)
+  args = vars(args)
+  infile = args.pop('infile')
+  nomvars = args.pop('v')
+
+  # Apply message level criteria.
+  fstopt ('MSGLVL',6)
+  fstd2nc.stdout.streams = ('error',)
+
+  try:
+    buf = buffer_type(infile, **args)
+  except FSTDError:
+    error (_("problem opening one or more input files."))
+
+  # Get the metadata in a netCDF-like structure.
+  x = buf.to_xarray()
+
+  # Print header info.
+  x.info()
+  print ()
+
+  # Print variable data.
+  if nomvars is not None:
+    nomvars = [n for nomvar in nomvars for n in nomvar.split(',')]
+    for nomvar in nomvars:
+      if nomvar in x:
+        values = x[nomvar].values
+        if 'bytes' in values.dtype.name:
+          values = values.astype(str)
+        print ("\n%s = %s ;"%(nomvar,values))
+
+
+#################################################
 # Command-line invocation of the converter.
 def _fstd2nc_cmdline (buffer_type=Buffer):
   from argparse import ArgumentParser
@@ -100,6 +147,7 @@ def _fstd2nc_cmdline (buffer_type=Buffer):
 
   buf.to_netcdf(outfile, nc_format=nc_format, global_metadata=global_metadata, zlib=zlib, compression=compression, progress=progress)
 
+#################################################
 # Command-line invocation with error trapping.
 # Hides the Python stack trace when the user aborts the command.
 def _fstd2nc_cmdline_trapped (*args, **kwargs):
