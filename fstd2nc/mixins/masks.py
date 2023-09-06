neishm/fstd2nc

commit 12f65f4245ac9fe98ace7c761ea56e42e9e65e6e

Clean up masks mixin - no longer have the "easy" case implemented, where it's assumed the mask and data records are interleaved.

fstd2nc/mixins/masks.py
@@ -82,16 +82,9 @@ class Masks (BufferBase):
 
     nrecs = len(self._headers['name'])
 
-    has_mask = (nomvar[:-1] == nomvar[1:]) & (etiket[:-1] == etiket[1:]) & (datev[:-1] == datev[1:]) & (ip1[:-1] == ip1[1:]) & (ip2[:-1] == ip2[1:]) & (ip3[:-1] == ip3[1:]) & uses_mask[:-1] & uses_mask[1:] & (dltf[:-1] == dltf[1:])
-    has_mask = np.where(has_mask)[0]
-
     # Figure out how to pair up the data and mask.
     # Requires O(n log n) time, which is better than O(n^2) for naive lookup
     # on each record.
-    # Easy case - masks appear immediately following the data.
-    # (Removed in current implementation, no longer reading mask and data together).
-
-    # Harder case - masks are in the file, but in random order.
 
     keys = OrderedDict([('dltf',dltf),('nomvar',nomvar),('etiket',etiket),('datev',datev),('ip1',ip1),('ip2',ip2),('ip3',ip3),('typvar',typvar)])
     ind = np.argsort(structured_array(keys))
@@ -106,7 +99,6 @@ class Masks (BufferBase):
     ip2 = ip2[ind]
     ip3 = ip3[ind]
     dltf = dltf[ind]
-    uses_mask = np.array(typvar,dtype='|S2').view('|S1').reshape(-1,2)[:,1] == b'@'
     has_mask = (nomvar[:-1] == nomvar[1:]) & (etiket[:-1] == etiket[1:]) & (datev[:-1] == datev[1:]) & (ip1[:-1] == ip1[1:]) & (ip2[:-1] == ip2[1:]) & (ip3[:-1] == ip3[1:]) & uses_mask[:-1] & uses_mask[1:] & (dltf[:-1] == dltf[1:])
     has_mask = np.where(has_mask)[0]
     has_mask += 1  # Data appears after mask in this case.

commit 9c97beb29110c25c7b8033ea0740f0c38f37dfd1

Fix a bug when checking for masked data, which was causing false negatives in some cases.
The bug was introduced in recent commit 12f65f4245ac9fe98ace7c761ea56e42e9e65e6e
during some code cleanup.

fstd2nc/mixins/masks.py
@@ -103,6 +103,7 @@ class Masks (BufferBase):
     ip2 = ip2[ind]
     ip3 = ip3[ind]
     dltf = dltf[ind]
+    uses_mask = np.array(typvar,dtype='|S2').view('|S1').reshape(-1,2)[:,1] == b'@'
     has_mask = (nomvar[:-1] == nomvar[1:]) & (etiket[:-1] == etiket[1:]) & (datev[:-1] == datev[1:]) & (ip1[:-1] == ip1[1:]) & (ip2[:-1] == ip2[1:]) & (ip3[:-1] == ip3[1:]) & uses_mask[:-1] & uses_mask[1:] & (dltf[:-1] == dltf[1:])
     has_mask = np.where(has_mask)[0]
     has_mask += 1  # Data appears after mask in this case.