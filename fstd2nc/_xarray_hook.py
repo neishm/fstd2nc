4cdda29abde465d2944dc09ac8dd247030b63964
Mike Neish <Michael.Neish@ec.gc.ca>
2023-10-30 17:44:31 +0000

    Start on interface for accessing data using information from the index file.
    So far, have metadata and array structure being define.  No actual reading of
    data yet.

--- fstd2nc/_xarray_hook.py
+++ fstd2nc/_xarray_hook.py
@@ -1,5 +1,55 @@
 import fstd2nc
-from xarray.backends import BackendEntrypoint
+from xarray.backends import BackendEntrypoint, BackendArray
+
+# Create a lazy array interface from the given index data.
+class FSTDBackendArray(BackendArray):
+  __slots__ = ("shape", "dtype", "_buffer", "_files", "_args")
+  def __init__ (self, buffer, files, group):
+    self._buffer = buffer
+    self._files = files
+    template = group.variables['template']
+    self.shape = template.shape
+    self.dtype = template.dtype
+  def __getitem__ (self, key):
+    raise NotImplementedError("Can't read data yet!")
+# Create a dataset from the given index file.
+def open_index (buffer, indexfile):
+  # Get dimensions / coordinates.
+  import xarray as xr
+  import netCDF4 as nc
+  # Open with xarray to get axes / coordinates.
+  ds = xr.open_dataset(indexfile)
+  # Open with netCDF4 get get access to the group structures containing the
+  # metadata.
+  root = nc.Dataset(indexfile, 'r')
+  files = root.variables['files']
+  # Generate the variables.
+  vardict = {}
+  for varname, group in root.groups.items():
+    # Get a lazy accessor for the data.
+    array = FSTDBackendArray (buffer, files, group)
+    array = xr.core.indexing.LazilyIndexedArray(array)
+    # Define the preferred chunking for the data, based on how it's stored
+    # on disk.
+    preferred_chunks = {}
+    all_dims = group.variables['template'].dimensions
+    outer_dims = group.groups['data'].variables['address'].dimensions
+    for dim in all_dims:
+      if dim in outer_dims:
+        preferred_chunks[dim] = 1
+      else:
+        preferred_chunks[dim] = len(root.dimensions[dim])
+    encoding = {'preferred_chunks':preferred_chunks}
+    # Annotate the variable with dimensions and other metadata.
+    template = group.variables['template']
+    vardict[varname] = xr.Variable (template.dimensions, array, attrs=template.__dict__, encoding=encoding)
+  # Put these variables into the Dataset structure (where the coordinates are
+  # already defined.
+  ds = ds.merge(vardict)
+  # Remove bookkeeping variables.
+  del ds['files']
+  del ds.attrs['version']
+  return (ds)
 
 # Quick and dirty hack to get list of parameters that could be passed to
 # to Buffer interface.
@@ -10,7 +60,7 @@ def _get_open_dataset_parameters ():
   parser = argparse.ArgumentParser()
   fstd2nc.Buffer._cmdline_args(parser)
   args = parser.parse_args([])
-  return ('filename_or_obj', 'drop_variables', 'fused', 'batch', 'cachefile') + tuple(vars(args).keys())
+  return ('filename_or_obj', 'drop_variables', 'fused') + tuple(vars(args).keys())
 
 # Register this as a backend for xarray, so FST files can be directly
 # opened with xarray.open_dataset
