Fix issue with integer overflow when traversing large FST files with numpy 2.x.
Multiplying large addresses by 8 (when they're already near the 32-bit limit)
is causing an in integer overflow, and thus giving an invalid address.

In numpy 1.x, these values would be upcasted to int64 automatically, but in
numpy 2.x, it just overflows.

fstd2nc/extra.py
@@ -372,7 +372,7 @@ def _raw_headers_fst (f):
     raw.append(params[:nent].view('B').flatten())
     recno_list.extend(list(range(nent)))
     pageno_list.extend([pageno]*nent)
-    pageaddr = page[4]; pageno += 1
+    pageaddr = int(page[4]); pageno += 1
   raw = np.concatenate(raw)
   f.close()
   return raw.reshape(-1,72)